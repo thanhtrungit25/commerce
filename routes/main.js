6ecc0d171c554de2c7f92c5f66df1ddac619b180
Thanh Trung <thanhtrung.it25@gmail.com>
2016-07-27 21:47:02 +0700

    Adding router get products follow category id

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,5 @@
 var router = require('express').Router();
-var User = require('../models/user');
+var Product = require('../models/product')
 
 router.get('/', function (req, res) {
   res.render('main/home');
@@ -9,10 +9,16 @@ router.get('/about', function (req, res) {
   res.render('main/about');
 })
 
-router.get('/users', function (req, res) {
-  User.find({}, function (err, users) {
-    res.json(users);
-  })
-})
+router.get('/products/:id', function (req, res, next) {
+  Product
+    .find({category: req.params.id})
+    .populate('category')
+    .exec(function (err, products) {
+      if (err) return next(err);
+      res.render('main/category', {
+        products: products
+      });
+    });
+});
 
 module.exports = router;
