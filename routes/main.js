5cdb48644c730e5a29e23e6fa54541c512dc9df4
Thanh Trung <thanhtrung.it25@gmail.com>
2016-07-28 21:34:33 +0700

    Adding cart page

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,6 @@
 var router = require('express').Router();
-var Product = require('../models/product')
+var Product = require('../models/product');
+var Cart = require('../models/cart');
 
 function paginate(req, res, next) {
   var perPage = 9;
@@ -53,21 +54,24 @@ router.get('/cart', function (req, res, nex) {
     .populate('items.item')
     .exec(function (err, foundCart) {
       if (err) return next(err);
-      res.render('main/cart', function (
-        cart: foundCart
-      ));
+      // res.json(foundCart);
+      res.render('main/cart', {
+        foundCart: foundCart
+      });
     });
 });
 
 router.post('/product/:product_id', function (req, res, next) {
+  // res.json(req.user);
   Cart.findOne({ owner: req.user._id }, function (err, cart){
     cart.items.push({
       item: req.body.product_id,
       quantity: parseFloat(req.body.quantity),
-      price: parseFloat(req.body.priveValue)
+      price: parseFloat(req.body.priceValue)
     });
 
-    cart.total = (cart.total + parseFloat(req.body.priceValue)).toFixed(2);
+    cart.total = (cart.total + (parseFloat(req.body.quantity) * parseFloat(req.body.priceValue))).toFixed(2);
+    // res.json(cart);
 
     cart.save(function (err) {
       if (err) return next(err);
