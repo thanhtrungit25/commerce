03a4a3841a9e70ba39044f9b8fc90cb15d456ca9
Thanh Trung <thanhtrung.it25@gmail.com>
2016-07-28 14:37:05 +0700

    Adding pagination feature

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,27 @@
 var router = require('express').Router();
 var Product = require('../models/product')
 
+function paginate(req, res, next) {
+  var perPage = 9;
+  var page = req.params.page;
+
+  Product
+    .find()
+    .skip( perPage * page )
+    .limit(perPage)
+    .populate('category')
+    .exec(function (err, products) {
+      if (err) return next(err);
+      Product.count().exec(function (err, count) {
+        if (err) return next(err);
+        res.render('main/product-main', {
+          products: products,
+          pages: count / perPage
+        });
+      });
+    });
+}
+
 Product.createMapping(function (err, mapping) {
   if (err) {
     console.log('error creating mapping');
@@ -32,27 +53,23 @@ router.post('/search', function (req, res) {
 
 router.get('/search', function (req, res) {
   if (req.query.q) {
-    Product.search({
-      query_string: {
-        query: req.query.q
-      }
-    }, function (err, results) {
-      if (err) return next(err);
-      var data = results.hits.hits.map(function (hit) {
-        return hit;
-      });
-      res.json({data: data});
-      // res.render('main/search-result', {
-      //   query: req.query.q,
-      //   data: data
-      // });
-    });
+
   }
 });
 
-router.get('/', function (req, res) {
-  res.render('main/home');
-})
+router.get('/', function (req, res, next) {
+
+  if (req.user) {
+    paginate(req, res, next);
+  } else {
+    res.render('main/home');
+  }
+
+});
+
+router.get('/page/:page', function (req, res, next) {
+  paginate(req, res, next);
+});
 
 router.get('/about', function (req, res) {
   res.render('main/about');
