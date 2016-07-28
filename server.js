84e071a7704cded18368d1791e6e2351198d3614
Thanh Trung <thanhtrung.it25@gmail.com>
2016-07-28 16:54:10 +0700

    Adding cart quantity middleware

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ var secret = require('./config/secret');
 var User = require('./models/user');
 var Category = require('./models/category');
 
+var cartLength = require('./middleware/middleware');
+
 var app = express();
 
 mongoose.connect(secret.database, function (err) {
@@ -44,6 +46,8 @@ app.use(function (req, res, next) {
   res.locals.user = req.user;
   next();
 });
+
+app.use(cartLength);
 
 app.use(function (req, res, next) {
   Category.find({}, function (err, categories) {
