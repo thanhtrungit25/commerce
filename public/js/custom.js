e16c89c8ac187be541658404b8d74ab0376277cd
Thanh Trung <thanhtrung.it25@gmail.com>
2016-07-28 15:40:03 +0700

    process ajax search results

--- public/js/custom.js
+++ public/js/custom.js
@@ -12,10 +12,26 @@ $(function() {
       },
       dataType: 'json',
       success: function (json) {
-        var data = results.hits.hits.map(function (hit) {
+        var data = json.hits.hits.map(function (hit) {
           return hit;
         })
-        console.log(data);
+        // console.log(data);
+
+        $('#searchResults').empty();
+        for (var i = 0; i < data.length; i++) {
+          var html = '';
+          html += '<div class="col-md-4">';
+          html += '<a href="/product/' + data[i]._source._id + '">';
+          html += '<div class="thumbnail">';
+          html += '<img src="' + data[i]._source.image + '">';
+          html += '<div class="caption">';
+          html += '<h3>' + data[i]._source.name + '</h3>';
+          html += '<p>' + data[i]._source.category.name + '</p>';
+          html += '<p>$' + data[i]._source.price + '</p>';
+          html += '</div></div></a></div>';
+
+          $('#searchResults').append(html);
+        }
 
       },
 
